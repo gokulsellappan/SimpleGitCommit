07eb4aa664e428bfa63823cbf8236ed4c70f8440
gokulsellappan <gokulsellappan@gmail.com>
2020-08-26 14:52:59 +0530

    commit

--- src/test/java/mavenproject/loginCheck.java
+++ src/test/java/mavenproject/loginCheck.java
@@ -5,12 +5,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class loginCheck {
 	
 	
-	public static WebDriver driver = new ChromeDriver();
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setup() {
+		
+		driver= new ChromeDriver();
+	}
 	
 	
 	@Test
