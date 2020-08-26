3e17ee96cfc0a996833f96113da63cecdb642b68
gokulsellappan <gokulsellappan@gmail.com>
2020-08-26 13:01:13 +0530

    login file change

--- src/test/java/mavenproject/loginCheck.java
+++ src/test/java/mavenproject/loginCheck.java
@@ -10,12 +10,13 @@ import org.testng.annotations.Test;
 public class loginCheck {
 	
 	
-	public static WebDriver driver;
+	public static WebDriver driver = new ChromeDriver();
+	
 	
 	@Test
 	public void doLogin() {
 		
-		driver = new ChromeDriver(); 
+		
 		driver.get("https://www.facebook.com/");
 		driver.findElement(By.id("email")).sendKeys("ggokull@gmail.com");;
 		driver.findElement(By.id("pass")).sendKeys("gokul.s");
