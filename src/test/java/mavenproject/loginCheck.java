bceaf1c6c721e884ab87768a7ea1d3cb5f4df827
gokulsellappan <gokulsellappan@gmail.com>
2020-08-26 16:45:29 +0530

    new file

--- src/test/java/mavenproject/loginCheck.java
+++ src/test/java/mavenproject/loginCheck.java
@@ -4,6 +4,7 @@ package mavenproject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
